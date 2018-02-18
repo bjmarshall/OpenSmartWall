ecf2372af05c0f1752b67e3ffd32394383e4e07c
Ben Marshall <marsh.b.000@gmail.com>
2018-02-17 22:03:29 -0500

    Switched back to cv2 because cv3 is not a real module

--- vision.py
+++ vision.py
@@ -1,42 +1,42 @@
-import cv3
+import cv2
 import numpy as np
 import time
 
 FPS = 24  #Change as desired
 WAIT = int(1000 / FPS)
 
-cap = cv3.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 time.sleep(3)
 
 #Grab 30 images to get the camera adjusted and store a picture.
-for dummy in range(200):
+for dummy in range(30):
 	ret, original = cap.read()
 
-fgbg = cv3.createBackgroundSubtractorMOG()
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
-grayOriginal = cv3.cvtColor(original, cv3.COLOR_BGR2GRAY)
-original = cv3.flip(original, 1)
+grayOriginal = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+original = cv2.flip(original, 1)
 
 while True:
 	ret, img = cap.read()	#capture image
 
-	img = cv3.flip(img, 1)	#mirror image (for frontcams)
+	img = cv2.flip(img, 1)	#mirror image (for frontcams)
 	
-	#cv3.imshow('Original',img)
+	#cv2.imshow('Original',img)
 
-	gray = cv3.cvtColor(img, cv3.COLOR_BGR2GRAY)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 	difference = fgbg.apply(img, learningRate=0.000)
-	ret, thresh = cv3.threshold(difference, 200, 255, cv3.THRESH_BINARY)
+	ret, thresh = cv2.threshold(difference, 200, 255, cv2.THRESH_BINARY)
 
-	cv3.imshow('Original', original)
-	cv3.imshow('Current', img)
-	cv3.imshow('Difference', difference)
-	cv3.imshow('Adjustment', thresh)
+	cv2.imshow('Original', original)
+	cv2.imshow('Current', img)
+	cv2.imshow('Difference', difference)
+	cv2.imshow('Adjustment', thresh)
 
 	
-	difference2 = cv3.subtract(img, original)
+	difference2 = cv2.subtract(img, original)
 
 	### Alter intensity
 	phi = 1
@@ -44,29 +44,29 @@ while True:
 	maxIntensity = 255
 
 	difference2 = (difference2**2)/255
-	ret, difference2 = cv3.threshold(difference2, 1, 255, cv3.THRESH_BINARY)
+	ret, difference2 = cv2.threshold(difference2, 1, 255, cv2.THRESH_BINARY)
 
-	#cv3.imshow('Difference', difference)
-	#cv3.imshow('Adjustment 1', difference1)
-	#cv3.imshow('Adjustment 2', difference2)
-	#cv3.imshow('Original', original)
+	#cv2.imshow('Difference', difference)
+	#cv2.imshow('Adjustment 1', difference1)
+	#cv2.imshow('Adjustment 2', difference2)
+	#cv2.imshow('Original', original)
 
 	
-	corners = cv3.goodFeaturesToTrack(gray, 100, 0.01, 10)
+	corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
 	corners = np.int0(corners)
 
-	edges = cv3.Canny(gray,100,200)
+	edges = cv2.Canny(gray,100,200)
 
 	for i in corners:
 	    x,y = i.ravel()
-	    cv3.circle(img,(x,y),3,255,-1)	
+	    cv2.circle(img,(x,y),3,255,-1)	
 	
-	#cv3.imshow('Corners',img)
-	#cv3.imshow('Edges', edges)
-	k = cv3.waitKey(WAIT) & 0xff
+	#cv2.imshow('Corners',img)
+	#cv2.imshow('Edges', edges)
+	k = cv2.waitKey(WAIT) & 0xff
 
 	if k == 27:
 		break
 
 cap.release()
-cv3.destroyAllWindows()
+cv2.destroyAllWindows()
